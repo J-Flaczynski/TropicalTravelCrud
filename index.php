f5bfbbe75eca225272fde99c44000c70b381713f
odinVos <1197607@student.roc-nijmegen.nl>
2023-05-15 12:46:27 +0200

    les 1
    korte opzet met: background, navbar en "button"

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+<body>
+<header>
+<nav>
+<div class="logo">
+<img src="img/logo.png" alt="tropical logo" />
+</div>
+<div class="links">
+<a href="#"><h1>Booked</h1></a>
+<a href="#"><h1>Flights</h1></a>
+<a href="#"><h1>Gallery</h1></a>
+<a href="#"><h1>Messages</h1></a>
+<a href="#"><h1>Sign out</h1></a>
+</div>
+</nav>
+</header>
+    <main>
+        <div class="background">
+            <h1>Your journey</h1>
+            <h1>Our job</h1>
+            <br>
+            <div class="test">
+            Book flights
+            </div>
+        </div>
+    </main>
+</body>
+</html>
