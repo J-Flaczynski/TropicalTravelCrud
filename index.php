2bbf335ab3b7db9a3938a30e59877760060e2b4b
odinVos <1197607@student.roc-nijmegen.nl>
2023-05-16 11:39:43 +0200

    les 2
    verder gewerkt aan index pagina, aanpassingen: logo in navbar verkleint, buttons toegevoegd

--- index.php
+++ index.php
@@ -4,33 +4,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Document</title>
 </head>
 <body>
-<header>
-<nav>
-<div class="logo">
-<img src="img/logo.png" alt="tropical logo" />
-</div>
-<div class="links">
-<a href="#"><h1>Booked</h1></a>
-<a href="#"><h1>Flights</h1></a>
-<a href="#"><h1>Gallery</h1></a>
-<a href="#"><h1>Messages</h1></a>
-<a href="#"><h1>Sign out</h1></a>
-</div>
-</nav>
-</header>
+    <header>
+    <nav>
+    <div class="logo">
+    <img src="img/logo.png" alt="tropical logo" />
+    </div>
+    <div class="links">
+    <a href="#"><h1>Book</h1></a>
+    <a href="#"><h1>Gallery</h1></a>
+    <a href="#"><h1>About us</h1></a>
+    <a href="#"><h1>Contact</h1></a>
+    </div>
+    </nav>
+    </header>
     <main>
         <div class="background">
             <h1>Your journey</h1>
             <h1>Our job</h1>
             <br>
-            <div class="test">
+            <div class="book-button">
             Book flights
             </div>
         </div>
+        <div class="content">
+            <h2>Fast and reliable</h2>
+            <span>Helped houndreds of travelers across the globe.</span>
+            <div class="comments">
+                <div><span>1</span></div>
+                <div><span>2</span></div>
+                <div><span>3</span></div>
+            </div>
+            <div class="book-adventure">
+            Book your adventure 
+            </div>
+        </div>
     </main>
+    <footer>
+    <div class="logo">
+    <img src="img/logo.png" alt="tropical logo" />
+    </div>
+    <div class="links">
+    <a href="#"><h1>Booked</h1></a>
+    <a href="#"><h1>Flights</h1></a>
+    <a href="#"><h1>Gallery</h1></a>
+    <a href="#"><h1>Messages</h1></a>
+    <a href="#"><h1>Sign out</h1></a>
+    </div>
+    </footer>
 </body>
 </html>
